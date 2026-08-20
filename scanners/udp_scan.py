68f16a3e8a1b8f28c6460fc9151f9fe3e5bc3764
cybermaksx <cybermaksx@proton.me>
2026-08-20 21:17:55 +0300

    Just made sceleton of udp_scan.py . Comming really sooon . I want to make my network_mapper.py a little bit stronger

---
+++ scanners/udp_scan.py
@@ -0,0 +1,68 @@
+import socket 
+import struct
+import os
+'''
+I am not going to spend much time for this script
+
+The purpose is reminding myself how UDP scan works so i can add to my network_mapper.py
+
+You can use it just to understand how  
+    sudo nmap -sU -p1002 
+
+works underhood
+
+And i would be incredibly happy if this helps you to learn something new
+
+'''
+
+
+
+target_ip = input("\n What is your target's ip ?\n ")
+
+port = ("Which port you are going to scan?") 
+
+
+
+
+def udp_scan(target_ip, ports):
+    pass
+
+
+
+
+
+
+
+
+
+udp_scan(target_ip, port)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
