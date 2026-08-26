faeadcfebb5ae020c6b36c5af08224b776c4a403
cybermaksx <cybermaksx@proton.me>
2026-08-27 00:44:01 +0300

    run function creates process and Arper class relay on it

--- spoofers/arp_poisoning.py
+++ spoofers/arp_poisoning.py
@@ -32,7 +32,12 @@ class Arper:
 
 
 def run (self):
-    pass
+    self.poison_thread = Process(target=self.poison)
+    self.poison_thread.start()
+
+    self.sniff_thread = Process(target=self.sniff)
+    self.sniff_thread.start()
+    
 
 
 def poison(self):
