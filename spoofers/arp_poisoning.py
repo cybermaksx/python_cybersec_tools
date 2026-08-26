a09577f7aa57281e48a213bb40968bfa5ab97b8e
cybermaksx <cybermaksx@proton.me>
2026-08-27 00:22:26 +0300

    get_mac function can get ARP of our target and send packets to a broadcast

--- spoofers/arp_poisoning.py
+++ spoofers/arp_poisoning.py
@@ -7,7 +7,11 @@ import sys
 import time
 
 def get_mac(targetip):
-    pass
+    packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op="who-has" pdst=targetip)
+    resp, _ = srp(packet, timeout= 2 , retry= 10 , verbose = False)
+    for _, r in resp:
+        return r[Ether].src
+    return None
 
 
 class Arper:
